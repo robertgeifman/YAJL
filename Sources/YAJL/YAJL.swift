13aee7d4f012086fd7f08aace2169677b178486b
Robert Geifman <robertgeifman@me.com>
2020-08-26 13:21:43 +0200

    wip
    Signed-off-by: Robert Geifman <robertgeifman@me.com>

--- Sources/YAJL/YAJL.swift
+++ Sources/YAJL/YAJL.swift
@@ -1,24 +1,14 @@
 @_exported import yajl_objc
 
-// MARK: - YAJLParserStatus
-extension YAJLParserStatus: CustomStringConvertible {
-	public var description: String {
-		switch rawValue {
-		case 0: return "none"
-		case 1: return "ok"
-		case 2: return "insufficientData"
-		case 3: return "error"
-		default: return "\(rawValue)"
-		}
-	}
-}
-
-// MARK: - YAJLParser
-public extension YAJLParser {
-}
-
-// MARK: - <# Mark Description #>
+// MARK: - YAJLDocument
 public extension YAJLDocument {
+	internal final class MyDelegate: NSObject, YAJLDocumentDelegate {
+		var entries = [Any]()
+		func document(_ document: YAJLDocument, didAdd dictionary: [AnyHashable: Any]) {
+			guard nil == document.currentKeyPath else { return }
+			entries.append(dictionary)
+	    }
+	}
     convenience init(data: Data, parserOptions: YAJLParserOptions) throws {
 		var error: NSError?
 		self.init(data: data, parserOptions: parserOptions, error: &error)
@@ -33,6 +23,8 @@ public extension YAJLDocument {
 			throw error
 		}
     }
+
+	@discardableResult
 	func parse(_ data: Data) throws -> YAJLParserStatus {
 		var error: NSError?
 		let result = parse(data, error: &error)
@@ -40,5 +32,28 @@ public extension YAJLDocument {
 			throw error
 		}
 		return result
+	}
+	func parseRootEntries(_ data: Data) throws -> [Any] {
+		let previousDelegate = delegate
+		defer { delegate = previousDelegate }
+
+		let myDelegate = MyDelegate()
+		delegate = myDelegate
+
+		try parse(data)
+		return myDelegate.entries
+	}
+}
+
+// MARK: - YAJLParserStatus
+extension YAJLParserStatus: CustomStringConvertible {
+	public var description: String {
+		switch rawValue {
+		case 0: return "none"
+		case 1: return "ok"
+		case 2: return "insufficientData"
+		case 3: return "error"
+		default: return "\(rawValue)"
+		}
 	}
 }
